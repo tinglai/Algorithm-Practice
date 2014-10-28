45f906e5b6396fb957838004ffc4a8fb22d9a52b
Ting <laiting@umich.edu>
2014-10-28 10:33:29 -0400

    add followup for chap9-3 to deal with the case of nondistinct elements in the array

--- CC150/chpt9/3/main.cpp
+++ CC150/chpt9/3/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
 int search(int, int,vector<int>&);
+int search_followup(int, int,vector<int>&);
 
 int main(){
 	vector<int> array = {-1, 0, 1, 2, 4, 6, 7};
 	int result = search(0, array.size() - 1, array);
 	if(result != -1){
 		cout << "The magic index is " << result << endl;
+	}
+	else{
+		cout << "Don't find the magic index" << endl;
+	}
+	cout << "*** follow up ***" << endl;
+	vector<int> array_followup = {-1, 0, 4, 4, 5, 5, 7};
+	int result_followup = search_followup(0, array_followup.size() - 1, array_followup);
+	if(result_followup != -1){
+		cout << "The magic index is " << result_followup << endl;
 	}
 	else{
 		cout << "Don't find the magic index" << endl;
@@ -32,4 +43,22 @@ int search(int a, int b, vector<int>& array){
 		return search(mid + 1, b, array);
 	}
 	else return search(a, mid - 1, array);
+}
+
+int search_followup(int a, int b, vector<int>& array){
+//this is for the array that the elements are not unique (distinct)
+	int mid = (a + b) / 2;
+	if(array[mid] == mid) return mid;
+	else if(a == b || a > b){
+		return -1;//return -1 means search fails
+	}
+	else{
+		int right = search_followup(max(mid + 1, array[mid]), b, array);
+		if(right != -1){
+			return right;
+		}
+		else{
+			return search_followup(a, min(mid - 1, array[mid]), array);
+		}
+	}
 }
